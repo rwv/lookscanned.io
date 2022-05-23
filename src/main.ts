e0923bc5ff40b7a9587b8a011080ff77afab3e2e
Seedgou <i@zczc.cz>
2022-05-23 11:43:31 +0000

    don't load web Fonts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
+// import { loadFonts } from "./plugins/webfontloader";
 import { createMetaManager } from "vue-meta";
 
-loadFonts();
+// loadFonts();
 
 createApp(App).use(router).use(createMetaManager()).use(vuetify).mount("#app");
