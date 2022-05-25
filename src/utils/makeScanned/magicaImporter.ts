04fe637a57d6913df504faf69cb0bcaea4eae85a
Seedgou <i@zczc.cz>
2022-05-25 15:13:52 +0000

    import magica wasm using vite static url

--- src/utils/makeScanned/magicaImporter.ts
+++ src/utils/makeScanned/magicaImporter.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const global = self as any;
 
-global.MAGICA_WASM_LOCATION = "/vendors/magica/magick.wasm";
+import wasmURL from "magica/dist/src/imageMagick/compiled/magick.wasm?url";
+
+global.MAGICA_WASM_LOCATION = wasmURL;
 
 export { main } from "magica-re-export";
