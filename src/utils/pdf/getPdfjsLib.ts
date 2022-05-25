cfceb665c97898881d8717c79b4fbf67c5de24af
Seedgou <i@zczc.cz>
2022-05-25 15:19:36 +0000

    import pdf.js worker url using vite

--- src/utils/pdf/getPdfjsLib.ts
+++ src/utils/pdf/getPdfjsLib.ts
@@ -1,8 +1,10 @@
 type pdfjsLibType = typeof import("pdfjs-dist");
 
+import pdfJsWorkerURL from "pdfjs-dist/build/pdf.worker.min.js?url"
+
 export default async function getPdfjsLib(): Promise<pdfjsLibType> {
   // import pdfjs-dist from node_modules
   const pdfjsLib = await import("pdfjs-dist");
-  pdfjsLib.GlobalWorkerOptions.workerSrc = "/vendors/pdf.js/pdf.worker.min.js";
+  pdfjsLib.GlobalWorkerOptions.workerSrc = pdfJsWorkerURL;
   return pdfjsLib;
 }
