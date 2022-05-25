11539e21eecd90a63d5ffbcb54d8f2614ae8310a
Seedgou <i@zczc.cz>
2022-05-25 16:11:54 +0000

    remove unused file

--- src/utils/misc/arrayBufferViewToDataUri.ts
+++
@@ -1,9 +0,0 @@
-export default async (abv: ArrayBufferView, mime: string): Promise<string> => {
-  const imageBlob = new Blob([abv], { type: mime });
-  const dataUri: string = await new Promise((r) => {
-    const reader = new FileReader();
-    reader.onload = () => r(reader.result as string);
-    reader.readAsDataURL(imageBlob);
-  });
-  return dataUri;
-};
--- src/utils/misc/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as dataUriToUint8Array } from "./dataUriToUint8Array";
-export { default as arrayBufferViewToDataUri } from "./arrayBufferViewToDataUri";
