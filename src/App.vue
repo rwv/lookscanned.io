6f3530477c4200726dd9ea25b85c9576482a2032
Seedgou <i@zczc.cz>
2022-05-26 02:05:40 +0000

    remove prefetch head

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,10 @@ import { useMeta } from "vue-meta";
 
 import NavBar from "@/components/NavBar.vue";
 
-const prefetchUrls = [
-  "/vendors/pdf.js/pdf.worker.min.js",
-  "/vendors/makeScanned/worker/processImage.worker.js",
-  "/vendors/makeScanned/worker/combineImagesToPdf.worker.js",
-  "/vendors/magica/magick.wasm",
-];
-
-const prefetchUrlsMeta = prefetchUrls.map((url) => ({
-  rel: "prefetch",
-  href: url,
-}));
-
 useMeta({
   title: "Look Scanned",
   description:
     "Look Scanned is a pure frontend site that makes your PDFs look scanned! No need for printers and scanners anymore - everything you need to do is just a few clicks.",
-  link: prefetchUrlsMeta,
 });
 </script>
 
