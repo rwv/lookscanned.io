89284a205d390faf30f68a5637b990369c4791a2
Seedgou <i@zczc.cz>
2022-05-27 16:06:50 +0000

    remove useless file

--- src/utils/pdf/getPdfDocument.ts
+++
@@ -1,8 +0,0 @@
-import getPdfjsLib from "./getPdfjsLib";
-
-export default async function getPdfDocument(pdfSource: string) {
-  const pdfjsLib = await getPdfjsLib();
-  const loadingTask = pdfjsLib.getDocument(pdfSource);
-  const pdfDocument = await loadingTask.promise;
-  return pdfDocument;
-}
