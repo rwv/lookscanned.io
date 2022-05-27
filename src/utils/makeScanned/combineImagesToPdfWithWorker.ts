15db8131bd85a03c74c5929ed9da31d678588680
Seedgou <i@zczc.cz>
2022-05-27 15:31:07 +0000

    add log for combineImages

--- src/utils/makeScanned/combineImagesToPdfWithWorker.ts
+++ src/utils/makeScanned/combineImagesToPdfWithWorker.ts
@@ -6,9 +6,14 @@ export type ToWorkerMessage = {
 
 export type FromWorkerMessge = Blob;
 
+import { getLogger } from "@/utils/log";
+
+const logger = getLogger(["scan", "imageMagick", "combineImages"]);
+
 export const combineImagesToPdfWithWorker: combineImagesToPdfFuncType =
   async function (imageArrayBufferViews) {
     if (window.Worker) {
+      logger.log("Web Worker is supported by your browser!");
       return await new Promise((resolve) => {
         const magicaWorker = new Worker(
           new URL("./worker/combineImagesToPdf.worker.ts", import.meta.url),
@@ -16,11 +21,14 @@ export const combineImagesToPdfWithWorker: combineImagesToPdfFuncType =
             type: "module",
           }
         );
+        logger.log("Start Web Worker");
 
         // Receive message
         magicaWorker.onmessage = (e) => {
           const blob = e.data as FromWorkerMessge;
+          logger.log(`Get PDF Blob from Web Worker, ${blob.size} bytes`);
           magicaWorker.terminate();
+          logger.log("Terminate Web Worker");
           resolve(blob);
         };
 
@@ -29,8 +37,12 @@ export const combineImagesToPdfWithWorker: combineImagesToPdfFuncType =
           imageArrayBufferViews,
         } as ToWorkerMessage;
         magicaWorker.postMessage(message);
+        logger.log("Send Images to Web Worker");
       });
     } else {
+      logger.log(
+        "Web Worker is not supported by your browser, fallback to main thread."
+      );
       const combineImagesToPdf = (await import(
         "./combineImagesToPdf"
       )) as unknown as combineImagesToPdfFuncType;
