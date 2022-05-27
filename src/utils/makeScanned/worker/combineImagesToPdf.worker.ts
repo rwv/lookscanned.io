6db389d1f31bcb79827fe4691936085403dc8928
Seedgou <i@zczc.cz>
2022-05-27 15:33:47 +0000

    add log for combine images worker

--- src/utils/makeScanned/worker/combineImagesToPdf.worker.ts
+++ src/utils/makeScanned/worker/combineImagesToPdf.worker.ts
@@ -5,13 +5,20 @@ import type {
   FromWorkerMessge,
 } from "../combineImagesToPdfWithWorker";
 
+import { getLogger } from "@/utils/log";
+
+const logger = getLogger(["scan", "imageMagick", "processImage", "webWorker"]);
+
 onmessage = function (e) {
-  console.log("Worker combineImagesToPdf: Received message from main script");
   const data = e.data as ToWorkerMessage;
   const imageArrayBufferViews = data.imageArrayBufferViews;
+  logger.log(`${imageArrayBufferViews.length} images received from main`);
 
   (async () => {
+    logger.log("Start combining images");
     const blob = await combineImagesToPdf(imageArrayBufferViews);
+    logger.log(`Finish combining images, size: ${blob.size}`);
     postMessage(blob as FromWorkerMessge);
+    logger.log("Send processed image to main");
   })();
 };
