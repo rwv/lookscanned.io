64e6cb62b776020c83d34db41539fe2613d8cf97
Seedgou <i@zczc.cz>
2022-05-28 12:08:49 +0000

    remove useless export

--- src/utils/makeScanned/index.ts
+++ src/utils/makeScanned/index.ts
@@ -1,11 +1,5 @@
-export { processImageWithWorker as processImage } from "./processImageWithWorker";
-
 export type { ScanConfig } from "./ScanConfig";
 
-export { makeScannedPdf } from "./makeScannedPdf";
-
 export { defaultConfig } from "./defaultConfig";
-
-export { colorspaces } from "./ScanConfig";
 
 export { Scan } from "./Scan";
