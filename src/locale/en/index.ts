b347ea9f8d4f71c8c1efa12654b51671c620d654
Seedgou <i@zczc.cz>
2022-06-02 08:26:30 +0000

    i18n add settings

--- src/locale/en/index.ts
+++ src/locale/en/index.ts
@@ -1,9 +1,11 @@
 import { base } from "./base";
 import { features } from "./features";
 import { actions } from "./actions";
+import { settings } from "./settings";
 
 export const en = {
   base,
   features,
   actions,
+  settings,
 };
