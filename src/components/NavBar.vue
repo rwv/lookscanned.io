7285003f8fbb5902baad83a8a5ad63dd29862739
Seedgou <i@zczc.cz>
2022-06-02 14:05:09 +0000

    add link to homepage

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,19 +7,28 @@
     </v-app-bar-title>
 
     <template v-slot:append>
+      <v-btn
+        :href="HOMEPAGE_URL"
+        @click.prevent="openHomepage"
+        :icon="mdiHome"
+      />
       <v-btn :href="GITHUB_URL" @click.prevent="openGitHub" :icon="mdiGithub" />
     </template>
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
-import { mdiGithub } from "@mdi/js";
+import { mdiGithub, mdiHome } from "@mdi/js";
 import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 
 const GITHUB_URL = "https://github.com/rwv/lookscanned.io";
 
 const openGitHub = () => window.open(GITHUB_URL, "_blank");
+
+const HOMEPAGE_URL = "https://zczc.cz/";
+
+const openHomepage = () => window.open(HOMEPAGE_URL, "_blank");
 </script>
 
 <style lang="scss" scoped>
