6df158a146276a5752f9fbfef823c38d9ad7dfd9
Seedgou <i@zczc.cz>
2022-06-02 02:47:19 +0000

    install i18n

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { createMetaManager } from "vue-meta";
+import { createI18n } from "vue-i18n";
 
-createApp(App).use(router).use(createMetaManager()).use(vuetify).mount("#app");
+const i18n = createI18n({
+  locale: "en",
+  fallbackLocale: "en",
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.use(createMetaManager());
+app.use(i18n);
+app.use(vuetify);
+
+app.mount("#app");
