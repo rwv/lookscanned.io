2c73a2738757c76f72087581350e012ab28d4de7
Seedgou <i@zczc.cz>
2022-08-16 10:42:31 +0000

    add color for colorspace switch

--- src/components/ScanSettings/Settings/ColorspaceSetting.vue
+++ src/components/ScanSettings/Settings/ColorspaceSetting.vue
@@ -1,6 +1,6 @@
 <template>
   <n-form-item :label="t('settings.colorspace.label')" :show-feedback="false">
-    <NSwitch v-model:value="colorspaceSwitch">
+    <NSwitch v-model:value="colorspaceSwitch" :rail-style="railStyle">
       <template #checked>{{ t("settings.colorspace.colorful") }}</template>
       <template #unchecked>{{ t("settings.colorspace.grayscale") }}</template>
     </NSwitch>
@@ -9,7 +9,7 @@
 
 <script lang="ts" setup>
 import type { ScanConfig } from "@/utils/scan";
-import { computed } from "vue";
+import { type CSSProperties, computed } from "vue";
 import { NFormItem, NSwitch } from "naive-ui";
 import { useI18n } from "vue-i18n";
 const { t } = useI18n();
@@ -28,4 +28,29 @@ const colorspaceSwitch = computed({
   get: () => props.colorspace !== "gray",
   set: (colorspace) => emit("update:colorspace", colorspace ? "sRGB" : "gray"),
 });
+
+const railStyle = ({
+  focused,
+  checked,
+}: {
+  focused: boolean;
+  checked: boolean;
+}) => {
+  const style: CSSProperties = {};
+  if (checked) {
+    // If colorspace is colorful, the rail is colored.
+    // get random color from array
+    style.background =
+      "linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)";
+    if (focused) {
+      style.boxShadow = "0 0 0 2px #05193740";
+    }
+  } else {
+    style.background = "#000000";
+    if (focused) {
+      style.boxShadow = "0 0 0 2px #00000040";
+    }
+  }
+  return style;
+};
 </script>
