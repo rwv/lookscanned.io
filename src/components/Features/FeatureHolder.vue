70a3ff1428edbb6e2b83e711eb4b9451d8afd62b
Seedgou <i@zczc.cz>
2022-08-16 13:03:15 +0000

    remove unused code

--- src/components/Features/FeatureHolder.vue
+++ src/components/Features/FeatureHolder.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-space vertical :size="0">
+  <div>
     <n-h3>
       <n-icon size="1em" :component="icon" style="margin-right: 0.5em" />{{
         title
@@ -8,12 +8,12 @@
     <n-p>
       <slot></slot>
     </n-p>
-  </n-space>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { Component } from "vue";
-import { NH3, NP, NSpace, NIcon } from "naive-ui";
+import { NH3, NP, NIcon } from "naive-ui";
 
 defineProps<{ title: string; icon: Component }>();
 </script>
