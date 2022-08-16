abe32116bf0cb33aa22c18000730a0b7336bce68
Seedgou <i@zczc.cz>
2022-08-16 11:29:31 +0000

    naive ui side by side preview

--- src/components/preview/SideBySidePreview.vue
+++ src/components/preview/SideBySidePreview.vue
@@ -1,19 +1,25 @@
 <template>
-  <v-row>
-    <v-col cols="12" md="6">
-      <v-card elevation="4">
-        <RawPreview :pdfInstance="pdfInstance" :page="page" />
-      </v-card>
-    </v-col>
-    <v-col cols="12" md="6">
-      <v-card elevation="4">
-        <scanned-preview :scanInstance="scanInstance" :page="page" />
-      </v-card>
-    </v-col>
-  </v-row>
+  <n-grid cols="1 m:2" x-gap="25px" responsive="screen">
+    <n-grid-item>
+      <n-card>
+        <template #cover>
+          <RawPreview :pdfInstance="pdfInstance" :page="page" />
+        </template>
+      </n-card>
+    </n-grid-item>
+    <n-grid-item>
+      <n-card>
+        <template #cover>
+          <scanned-preview :scanInstance="scanInstance" :page="page" />
+        </template>
+      </n-card>
+    </n-grid-item>
+  </n-grid>
 </template>
 
 <script lang="ts" setup>
+import { NGrid, NGridItem, NCard } from "naive-ui";
+
 import RawPreview from "./RawPreview.vue";
 import ScannedPreview from "./ScannedPreview.vue";
 import type { PDF } from "@/utils/pdf";
