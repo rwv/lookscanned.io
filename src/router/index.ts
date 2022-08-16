3eea336d5a676c1c98b5bb517a29ab6349a687eb
Seedgou <i@zczc.cz>
2022-08-16 14:35:58 +0000

    async router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import ScanView from "@/views/ScanView.vue";
-import IndexView from "@/views/IndexView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +6,12 @@ const router = createRouter({
     {
       path: "/",
       name: "index",
-      component: IndexView,
+      component: () => import("../views/IndexView.vue"),
     },
     {
       path: "/scan",
       name: "scan",
-      component: ScanView,
+      component: () => import("../views/ScanView.vue"),
     },
   ],
 });
