701d3482b157034052510486f4599e7ef3f13571
Seedgou <i@zczc.cz>
2022-08-16 08:59:00 +0000

    naive ui pdf selection

--- src/components/ScanSettings/PDFSelection.vue
+++ src/components/ScanSettings/PDFSelection.vue
@@ -1,62 +1,54 @@
 <template>
-  <v-list-item>
-    <v-file-input
-      accept="application/pdf"
-      :label="t('settings.pdfSelectLabel')"
-      density="compact"
-      truncate-length="21"
-      :hide-details="!props.noFileError"
-      :error="props.noFileError"
-      :error-messages="noFileErrorMessage"
-      @change="onFileChange"
-    />
-  </v-list-item>
+  <n-button text @click="onClick">
+    <template #icon>
+      <n-icon>
+        <Attach16Regular />
+      </n-icon>
+    </template>
+    <n-text v-if="filename">{{ filename }}</n-text>
+    <n-text type="error" v-else-if="noFileError">
+      {{ t("settings.pdfNoSelectMessage") }}
+    </n-text>
+    <n-text v-else>
+      {{ t("settings.pdfSelectLabel") }}
+    </n-text>
+  </n-button>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
+import { NButton, NIcon, NText } from "naive-ui";
+import { Attach16Regular } from "@vicons/fluent";
+import { fileOpen } from "browser-fs-access";
+
 import type { PDFInfoType } from "@/utils/pdf";
 import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 
-const props = defineProps<{
+defineProps<{
   noFileError: boolean;
 }>();
-
-const noFileErrorMessage = computed(() => {
-  if (props.noFileError) {
-    return t("settings.pdfNoSelectMessage");
-  } else {
-    return [];
-  }
-});
 
 const emit = defineEmits<{
   (e: "update:pdfInfo", info: PDFInfoType): void;
 }>();
 
 const blobUrl = ref("");
+const filename = ref("");
 
-function onFileChange(event: Event) {
-  const files = (event.target as HTMLInputElement).files;
-  if (files) {
-    if (blobUrl.value !== "") {
-      URL.revokeObjectURL(blobUrl.value);
-    }
-    const file = files[0];
-    const url = URL.createObjectURL(file);
-    blobUrl.value = url;
-    const info = {
-      source: url,
-      filename: file.name,
-    } as PDFInfoType;
-    emit("update:pdfInfo", info);
-  }
+async function onClick() {
+  const blob = await fileOpen({
+    description: "PDF Files",
+    mimeTypes: ["application/pdf"],
+    extensions: [".pdf"],
+  });
+  URL.revokeObjectURL(blobUrl.value);
+  blobUrl.value = URL.createObjectURL(blob);
+  filename.value = blob.name;
+  const info = {
+    source: blobUrl.value,
+    filename: filename.value,
+  };
+  emit("update:pdfInfo", info);
 }
 </script>
-
-<style>
-.v-file-input > div.v-input__control > div > div.v-field__overlay {
-  background-color: inherit;
-}
-</style>
