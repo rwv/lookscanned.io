2150f8dab90dbe49aeee14822c8b5aac6ce5b63b
Seedgou <i@zczc.cz>
2022-08-16 13:52:46 +0000

    use @vueuse/head instead of vue-meta

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createMetaManager } from "vue-meta";
 import i18n from "./locale";
 import VueGtag from "vue-gtag";
+import { createHead } from "@vueuse/head";
 
 const app = createApp(App);
+const head = createHead();
 
 app.use(router);
-app.use(createMetaManager());
+app.use(head);
 app.use(i18n);
 
 // Only for lookscanned.io
