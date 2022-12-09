6caa3e58c20b004eada9dc6a53bd9ee033ebfc5f
Seedgou <i@zczc.cz>
2022-12-09 08:17:23 +0000

    add initPromise for PDF

--- src/utils/pdf/PDF.ts
+++ src/utils/pdf/PDF.ts
@@ -17,11 +17,13 @@ export class PDF {
   private pdfDocument?: PDFDocumentProxy;
   private pageInfoCache: Map<number, pageInfoType> = new Map();
   readonly id: string;
+  private readonly initPromise: Promise<void>;
 
   constructor(pdfInfo: PDFInfoType) {
     this.pdfSource = pdfInfo.source;
     this.pdfFilename = pdfInfo.filename;
     this.id = this.pdfSource;
+    this.initPromise = this.init();
   }
 
   async init() {
@@ -31,66 +33,66 @@ export class PDF {
     this.pdfDocument = pdfDocument;
   }
 
-  async getDocument() {
-    if (this.pdfDocument) {
-      return this.pdfDocument;
-    } else {
-      await this.init();
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return this.pdfDocument!;
+  async getDocument(): Promise<PDFDocumentProxy> {
+    await this.initPromise;
+    if (!this.pdfDocument) {
+      throw new Error("PDF document is not initialized");
     }
+    return this.pdfDocument;
   }
 
-  async getNumPages() {
+  async getNumPages(): Promise<number> {
     const document = await this.getDocument();
     return document.numPages;
   }
 
-  async renderPage(page: number): Promise<pageInfoType> {
-    const scale = 2.0;
+  async renderPage(page: number, scale = 2.0): Promise<pageInfoType> {
     const dpi = scale * 72;
 
+    // Check if page is already in cache
     const pageInfoInCache = this.pageInfoCache.get(page);
     if (pageInfoInCache) {
       return pageInfoInCache;
-    } else {
-      const pdfDocument = await this.getDocument();
-      const pdfPage = await pdfDocument.getPage(page);
-      const viewport = pdfPage.getViewport({ scale });
-      const width = viewport.width;
-      const height = viewport.height;
-
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-      const ctx = canvas.getContext("2d");
-      const renderTask = pdfPage.render({
-        canvasContext: ctx as object,
-        viewport,
-      });
-      await renderTask.promise;
-
-      const blob: Blob = await new Promise((resolve, reject) => {
-        canvas.toBlob((blob) => {
-          if (blob) {
-            resolve(blob);
-          } else {
-            reject(new Error("Canvas to Blob failed"));
-          }
-        });
-      });
-
-      const pageInfo = {
-        blob,
-        page,
-        height,
-        width,
-        scale,
-        dpi,
-      };
-
-      this.pageInfoCache.set(page, pageInfo);
-      return pageInfo;
     }
+
+    const pdfDocument = await this.getDocument();
+    const pdfPage = await pdfDocument.getPage(page);
+    const viewport = pdfPage.getViewport({ scale });
+    const width = viewport.width;
+    const height = viewport.height;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d");
+    const renderTask = pdfPage.render({
+      canvasContext: ctx as object,
+      viewport,
+    });
+    await renderTask.promise;
+
+    const blob: Blob = await new Promise((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          resolve(blob);
+        } else {
+          reject(new Error("Canvas to Blob failed"));
+        }
+      });
+    });
+
+    canvas.remove();
+
+    const pageInfo = {
+      blob,
+      page,
+      height,
+      width,
+      scale,
+      dpi,
+    };
+
+    this.pageInfoCache.set(page, pageInfo);
+    return pageInfo;
   }
 }
