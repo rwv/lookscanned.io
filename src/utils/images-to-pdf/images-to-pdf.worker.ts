0148dc39693eb46be438a4cda49aa0461479959e
Seedgou <i@zczc.cz>
2022-12-10 16:34:13 +0000

    add type for images-to-pdf.worker

--- src/utils/images-to-pdf/images-to-pdf.worker.ts
+++ src/utils/images-to-pdf/images-to-pdf.worker.ts
@@ -1,8 +1,6 @@
 import { imagesToPDF } from "./images-to-pdf";
 import type { ImageInfo } from "./images-to-pdf";
 
-onmessage = async function (e) {
-  const images = e.data as ImageInfo[];
-  const pdfBlob = await imagesToPDF(images);
-  postMessage(pdfBlob);
+onmessage = async function (e: MessageEvent<ImageInfo[]>) {
+  postMessage(await imagesToPDF(e.data));
 };
