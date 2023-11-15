9b5745feac5938a772ba9fa894761053bc50d53a
Seedgou <i@zczc.cz>
2023-11-15 15:43:13 +0800

    use very simple cache

--- src/utils/scanner/scan-cacher/index.ts
+++ src/utils/scanner/scan-cacher/index.ts
@@ -4,7 +4,7 @@ type RenderPageResult = Awaited<ReturnType<ScanRenderer["renderPage"]>>;
 type RenderPageParams = Parameters<ScanRenderer["renderPage"]>;
 
 export class ScanCacher implements ScanRenderer {
-  private readonly cache = new Map<Blob, Promise<RenderPageResult>>();
+  private readonly cache = new Map<Blob, RenderPageResult>();
   private readonly renderer: ScanRenderer;
 
   constructor(renderer: ScanRenderer) {
@@ -15,27 +15,18 @@ export class ScanCacher implements ScanRenderer {
     image: RenderPageParams[0],
     options?: RenderPageParams[1]
   ): Promise<RenderPageResult> {
+    // TODO: we should cache Promise<Blob> instead of Blob
+    // however, we need to make sure that the Promise is not rejected
+    // otherwise, the cache will be stuck with a rejected Promise
+    // some mysterious error happens when we try to cache Promise<Blob>
+
     const cached = this.cache.get(image);
     if (cached) {
-      try {
-        const result = await cached;
-        return result;
-      } catch (e) {
-        if (
-          (e as Error).name === "AbortError" ||
-          (e as Error).message === "Aborted"
-        ) {
-          this.cache.delete(image);
-          const resultPromise = this.renderer.renderPage(image, options);
-          this.cache.set(image, resultPromise);
-          return await resultPromise;
-        }
-        throw e;
-      }
+      return cached;
     }
 
-    const resultPromise = this.renderer.renderPage(image, options);
-    this.cache.set(image, resultPromise);
-    return await resultPromise;
+    const result = await this.renderer.renderPage(image, options);
+    this.cache.set(image, result);
+    return result;
   }
 }
