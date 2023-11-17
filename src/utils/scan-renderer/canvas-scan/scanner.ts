5db2f174f6e2432e8f5dd41ca74b378a6bff7f1e
Seedgou <i@zczc.cz>
2023-11-17 09:28:58 +0800

    remove OffscreenCanvas for canvas-scan for better compatibility

--- src/utils/scan-renderer/canvas-scan/scanner.ts
+++ src/utils/scan-renderer/canvas-scan/scanner.ts
@@ -23,38 +23,24 @@ export class CanvasScanner implements ScanRenderer {
       throw new Error("Aborted");
     }
 
-    if ("OffscreenCanvas" in window) {
-      // TODO: use web worker
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const canvas = new OffscreenCanvas(10, 10);
-      await scanCanvas(canvas, image, this.config);
-      const blob = await canvas.convertToBlob({
-        type: this.config.output_format,
-      });
-      const height = canvas.height;
-      const width = canvas.width;
-      return { blob, height, width };
-    } else {
-      const canvas = document.createElement("canvas");
-      await scanCanvas(canvas, image, this.config);
-      if (options?.signal?.aborted) {
-        throw new Error("Aborted");
-      }
-
-      const blob = await new Promise<Blob>((resolve, reject) =>
-        canvas.toBlob((blob) => {
-          if (blob) {
-            resolve(blob);
-          } else {
-            reject(new Error("Canvas to Blob failed"));
-          }
-        }, this.config.output_format)
-      );
-      const height = canvas.height;
-      const width = canvas.width;
-      canvas.remove();
-      return { blob, height, width };
+    const canvas = document.createElement("canvas");
+    await scanCanvas(canvas, image, this.config);
+    if (options?.signal?.aborted) {
+      throw new Error("Aborted");
     }
+
+    const blob = await new Promise<Blob>((resolve, reject) =>
+      canvas.toBlob((blob) => {
+        if (blob) {
+          resolve(blob);
+        } else {
+          reject(new Error("Canvas to Blob failed"));
+        }
+      }, this.config.output_format)
+    );
+    const height = canvas.height;
+    const width = canvas.width;
+    canvas.remove();
+    return { blob, height, width };
   }
 }
